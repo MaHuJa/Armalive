c01e2715282d83ed92a0584a73ae590bb99fbe0c
MaHuJa <github@mahuja.net>
2014-07-04 22:23:59 +0200

    Script for updating file to latest DB changes

---
+++ database/dumpmasterschema.bat
@@ -0,0 +1,1 @@
+pg_dump.exe --host master1.armalive.com --port 5432 --username "mahuja" --format plain --schema-only --create --clean --no-tablespaces --verbose --file bstats_master.sql "bstats_master"
