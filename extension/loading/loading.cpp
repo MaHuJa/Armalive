faeb7aeebd5267cae6ea67ddbdbe29dc883fabfa
MaHuJa <github@mahuja.net>
2014-07-11 19:49:38 +0200

    Loader: new dll name, no obsolete functions

--- extension/loading/loading.cpp
+++ extension/loading/loading.cpp
@@ -9,14 +9,20 @@ int main(int argc, char* argv[])
 {
 	HMODULE m = LoadLibraryA(
 #ifdef _DEBUG
-		"..\\Debug\\bstats.dll"
+		"..\\Debug\\armalive.dll"
 #else
-		"..\\Release\\bstats.dll"
+		"..\\Release\\armalive.dll"
 #endif
 	);
-	if (!m) { std::cout << "LoadLibraryA failed\n";	return 1; }
+	if (!m) { 
+		std::cout << "LoadLibraryA failed\n";	
+		return 1; 
+	}
 	rve foo = reinterpret_cast<rve>(GetProcAddress(m,"_RVExtension@12"));
-	if (!foo) { std::cout << "No RVExtension()\n"; return 2; }
+	if (!foo) { 
+		std::cout << "No RVExtension()\n"; 
+		return 2; 
+	}
 	auto call = [foo](const char* str) {
 		char buf[256];
 		foo(buf, 256, str);
@@ -26,12 +32,13 @@ int main(int argc, char* argv[])
 	call("newplayer1;AAA;west;0;Testplayer A");
 	call("newplayer1;BBB;east;0;Testplayer B");
 	call("newplayer1;CCC;west;10;Testplayer C");
+	/* The function used here is obsolete.
 	call("inf_killed_inf1;AAA;BBB;15;testweapon1;10;[10,20,0];[20,10,0]");
 	call("inf_killed_inf1;AAA;BBB;15;testweapon1;10;[10,20,0];[20,10,0]");
 	call("inf_killed_inf1;BBB;CCC;15;testweapon2;10;[10,20,0];[20,10,0]");
 	call("inf_killed_inf1;CCC;AAA;15;testweapon1;10;[10,20,0];[20,10,0]");
 	call("inf_killed_inf1;AAA;BBB;15;testweapon2;10;[10,20,0];[20,10,0]");
-
+	*/
 	__asm nop;
 	std::this_thread::sleep_for(std::chrono::seconds(10));
 	return 0;
