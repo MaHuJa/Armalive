168a20ec2cf7ed741732fe18794edd69ff8b88e3
MaHuJa <github@mahuja.net>
2014-07-19 05:46:34 +0200

    Ex added squeue
    Synchronized queue - basically extracting the stuff from dbthread that
    deals with that.

---
+++ extension/armalive/squeue.h
@@ -0,0 +1,32 @@
+#pragma once
+
+template <class T>
+class squeue {
+	std::deque<T> content;
+	std::mutex qm;
+	typedef std::lock_guard<std::mutex> guard;
+public:
+
+	void push(T t) {
+		guard g(qm);
+		content.push_back(t);
+	}
+
+	// .second is true if there is an actual result
+	std::pair<T,bool> pop() {
+		std::pair<T, bool> ret;
+		guard g(qm);
+		if (content.empty()) {
+			ret.second = false;
+		}
+		else {
+			ret.first = move(content.front());
+			content.pop_front();
+			ret.second = true;
+		}
+		return ret;
+	}
+};
+
+
+
