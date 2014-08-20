653955b827d1af4e349bbaeb2ff0570057859ffe
MaHuJa <github@mahuja.net>
2014-08-21 00:27:51 +0200

    Database implements wpninfo1

--- database/server_wpninfo1.sql
+++ database/server_wpninfo1.sql
@@ -2,9 +2,26 @@
 
 -- DROP FUNCTION server.wpninfo1(integer, text, text, integer, integer, integer, integer, integer, integer, integer);
 
-CREATE OR REPLACE FUNCTION server.wpninfo1(sessionid integer, playerid text, vehicleclass text, "when" integer, weapontime integer, shotsfired integer, hit_head integer, hit_body integer, hit_arms integer, hit_legs integer)
+CREATE OR REPLACE FUNCTION server.wpninfo1
+(sessionid integer, playerid text, weaponclass text, "when" numeric, weapontime numeric, 
+ shotsfired integer, hit_vehicle integer, hit_head integer, hit_body integer, hit_arms integer, hit_legs integer)
   RETURNS void AS
 $BODY$
+  -- when is not actually used
+  -- upsert trigger
+  insert into player.weaponstats (session, player, class, totalseconds, 
+	fired, vehiclehits, headhits, bodyhits, leghits, armhits)
+  values
+  ($1,server.player_uid_to_id($2),$3,$5,
+  $6,$7,$8,$9,$10,$11);
+
+  insert into player.weaponstats_sum (player, class, totalseconds, 
+	fired, vehiclehits, headhits, bodyhits, leghits, armhits)
+  values
+  (server.player_uid_to_id($2),$3,$5,
+  $6,$7,$8,$9,$10,$11);
+
+  
 $BODY$
   LANGUAGE sql VOLATILE SECURITY DEFINER
   COST 100;
