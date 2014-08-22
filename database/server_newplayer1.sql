9c91ad6a702a1dad999b3f3b9b7406cecee74740
MaHuJa <github@mahuja.net>
2014-08-23 01:13:03 +0200

    Player names can now contain ; as part of the name
    Previously the db would reject them.

--- database/server_newplayer1.sql
+++ database/server_newplayer1.sql
@@ -2,15 +2,19 @@
 
 -- DROP FUNCTION server.newplayer1(integer, text, text, numeric, text);
 
-CREATE OR REPLACE FUNCTION server.newplayer1(sessionid integer, playeruid text, playerside text, jointime numeric, playername_p text)
+CREATE OR REPLACE FUNCTION server.newplayer1(sessionid integer, playeruid text, playerside text, jointime numeric, VARIADIC playername_p text[])
   RETURNS void AS
 $BODY$
-insert into player.player(gameuid,last_name_seen) values ($2, $5);
+DECLARE
+	pname text := array_to_string(playername_p,';');
+BEGIN
+insert into player.player(gameuid,last_name_seen) values ($2, pname);
 insert into session.sessionplayers(session, player, side, joined, playername) values 
-($1, server.player_uid_to_id($2), $3, server.seconds($4), $5);
+($1, server.player_uid_to_id($2), $3, server.seconds($4), pname);
 -- todo: add to playername list
+END
 $BODY$
-  LANGUAGE sql VOLATILE SECURITY DEFINER
+  LANGUAGE plpgsql VOLATILE SECURITY DEFINER
   COST 100;
 ALTER FUNCTION server.newplayer1(integer, text, text, numeric, text)
   OWNER TO armalive_auto;
