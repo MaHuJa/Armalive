b29d80de65cc097b6facef11cd5080b20eab1263
MaHuJa <github@mahuja.net>
2014-08-23 02:10:24 +0200

    Player name history now being recorded

--- database/server_newplayer1.sql
+++ database/server_newplayer1.sql
@@ -6,12 +6,19 @@ CREATE OR REPLACE FUNCTION server.newplayer1(sessionid integer, playeruid text, 
   RETURNS void AS
 $BODY$
 DECLARE
+	p_id integer := server.player_uid_to_id($2);
 	pname text := array_to_string(playername_p,';');
 BEGIN
+-- todo: Consider doing trigger work here
 insert into player.player(gameuid,last_name_seen) values ($2, pname);
 insert into session.sessionplayers(session, player, side, joined, playername) values 
-($1, server.player_uid_to_id($2), $3, server.seconds($4), pname);
--- todo: add to playername list
+  ($1, server.player_uid_to_id($2), $3, server.seconds($4), pname);
+
+-- add to playername
+update player.playername set lastseen = current_timestamp where playerid=p_id and name = pname;
+if not found then
+	insert into player.playername (playerid,name) values (p_id, pname);
+end if;
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE SECURITY DEFINER
