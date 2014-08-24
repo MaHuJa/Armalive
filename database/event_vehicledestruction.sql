0715be5b3c07d340468f2507c940a8f2a788cc25
MaHuJa <github@mahuja.net>
2014-08-25 01:12:51 +0200

    DB support for vehicle destruction
    Some unresolved questions may yet require changes to it.

--- database/event_vehicledestruction.sql
+++ database/event_vehicledestruction.sql
@@ -1,22 +1,36 @@
 ﻿-- Table: event.vehicledestruction
 
--- DROP TABLE event.vehicledestruction;
+DROP TABLE IF EXISTS event.vehicledestruction;
 
 CREATE TABLE event.vehicledestruction
 (
   eventid integer NOT NULL DEFAULT nextval('event.event_id_counter'::regclass),
   session integer NOT NULL,
-  extent character varying(8) NOT NULL, -- mobility, evacuated, decrewed, scrapped
-  killerid integer,
+  "when" interval NOT NULL,
+  severity character varying(9) NOT NULL, -- mobility, evacuated, decrewed, scrapped
+  vehicleclass text NOT NULL,
+  vehicleposition real[] NOT NULL,
+
+  last_used_by_side text,
+  last_used_by_player integer,
+  
+  killer integer,
   killer_position real[],
-  killer_weapon text,
-  target_position real[],
-  target_class text,
   killer_class text,
+  killer_weapon text,
+  killer_side text,
+  
   CONSTRAINT vehicledestruction_pkey PRIMARY KEY (eventid),
   CONSTRAINT vehicledestruction_session_fkey FOREIGN KEY (session)
       REFERENCES session.session (id) MATCH SIMPLE
-      ON UPDATE NO ACTION ON DELETE NO ACTION
+      ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT vehicledestruction_severity_check CHECK 
+	(severity::text = ANY (ARRAY[
+	'mobility'::character varying::text, 
+	'evacuated'::character varying::text, 
+	'decrewed'::character varying::text, 
+	'scrapped'::character varying::text
+	]))
 )
 WITH (
   OIDS=FALSE
@@ -26,5 +40,5 @@ ALTER TABLE event.vehicledestruction
 GRANT ALL ON TABLE event.vehicledestruction TO mahuja;
 GRANT INSERT ON TABLE event.vehicledestruction TO armalive_auto;
 GRANT SELECT ON TABLE event.vehicledestruction TO armalive_reader;
-COMMENT ON COLUMN event.vehicledestruction.extent IS 'mobility, evacuated, decrewed, scrapped';
+COMMENT ON COLUMN event.vehicledestruction.severity IS 'mobility, evacuated, decrewed, scrapped';
 
---
+++ database/server_veh_killed1.sql
@@ -0,0 +1,33 @@
+﻿-- Function: server.veh_killed1(integer, numeric, text, text, text, text, text, text, text, text, text, text)
+
+-- DROP FUNCTION server.veh_killed1(integer, numeric, text, text, text, text, text, text, text, text, text, text);
+
+CREATE OR REPLACE FUNCTION server.veh_killed1(sessionid integer, "when" numeric, severity text,
+vehicleclass text, vehicle_position text, 
+last_used_by_side text, last_used_by_player text,
+killer_uid text, killer_position text, 
+killer_class text, killer_weapon text, killer_side text
+)
+RETURNS void AS
+$BODY$
+INSERT INTO event.vehicledestruction(
+	session, "when", severity, 
+	vehicleclass, vehicleposition, 
+	last_used_by_side, last_used_by_player, 
+	killer, killer_position, 
+	killer_class, killer_weapon, killer_side
+	)
+    VALUES (
+	$1, server.seconds($2), $3, 
+	$4, server.position($5), 
+	$6, server.player_uid_to_id($7),
+	server.player_uid_to_id($8), server.position($9),
+	$10, $11, $12        
+	);
+$BODY$
+  LANGUAGE sql VOLATILE SECURITY DEFINER
+  COST 100;
+ALTER FUNCTION server.veh_killed1(integer, numeric, text, text, text, text, text, text, text, text, text, text)
+  OWNER TO armalive_auto;
+GRANT EXECUTE ON FUNCTION server.veh_killed1(integer, numeric, text, text, text, text, text, text, text, text, text, text) TO armalive_auto;
+GRANT EXECUTE ON FUNCTION server.veh_killed1(integer, numeric, text, text, text, text, text, text, text, text, text, text) TO armalive_server;
