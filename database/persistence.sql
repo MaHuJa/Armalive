0833cc4f864d2438b764ece3a1be3330cf01537e
MaHuJa <github@mahuja.net>
2014-09-08 22:53:13 +0200

    Atlas persistence database tables

---
+++ database/persistence.sql
@@ -0,0 +1,23 @@
+﻿-- Schema: persistence
+
+-- DROP SCHEMA persistence;
+
+CREATE SCHEMA persistence
+  AUTHORIZATION mahuja;
+
+GRANT ALL ON SCHEMA persistence TO mahuja;
+GRANT USAGE ON SCHEMA persistence TO armalive_auto;
+GRANT USAGE ON SCHEMA persistence TO armalive_admin;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA persistence
+    GRANT INSERT, SELECT, UPDATE ON TABLES
+    TO armalive_auto;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA persistence
+    GRANT SELECT, UPDATE, USAGE ON SEQUENCES
+    TO armalive_auto;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA persistence
+    GRANT EXECUTE ON FUNCTIONS
+    TO armalive_auto;
+
---
+++ database/persistence_atlas_archive.sql
@@ -0,0 +1,21 @@
+﻿-- Table: persistence.atlas_archive
+
+-- DROP TABLE persistence.atlas_archive;
+
+CREATE TABLE persistence.atlas_archive
+(
+  playerid integer NOT NULL,
+  varname text NOT NULL,
+  increment integer,
+  session integer NOT NULL,
+  id integer NOT NULL,
+  CONSTRAINT atlas_archive_pkey PRIMARY KEY (id)
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE persistence.atlas_archive
+  OWNER TO mahuja;
+GRANT ALL ON TABLE persistence.atlas_archive TO mahuja;
+GRANT SELECT, UPDATE, INSERT, DELETE ON TABLE persistence.atlas_archive TO armalive_auto;
+GRANT SELECT ON TABLE persistence.atlas_archive TO armalive_admin;
