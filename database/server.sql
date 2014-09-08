a00445f411998f78e458ca1896ad477e4fe91977
MaHuJa <github@mahuja.net>
2014-09-08 14:41:37 +0200

    Database changes to support #17
    Giving armalive_reader permissions to the uid->id function

--- database/server.sql
+++ database/server.sql
@@ -8,10 +8,15 @@ CREATE SCHEMA server
 GRANT ALL ON SCHEMA server TO mahuja;
 GRANT USAGE ON SCHEMA server TO armalive_auto;
 GRANT USAGE ON SCHEMA server TO armalive_server;
+GRANT USAGE ON SCHEMA server TO armalive_reader;
 COMMENT ON SCHEMA server
   IS 'This schema shall contain ALL the server needs access to. Mostly functions that run with enough priveleges for that particular task.';
 
 ALTER DEFAULT PRIVILEGES IN SCHEMA server
     GRANT EXECUTE ON FUNCTIONS
     TO armalive_auto;
+
+ALTER DEFAULT PRIVILEGES IN SCHEMA server
+    GRANT EXECUTE ON FUNCTIONS
+    TO armalive_server;
 
--- database/server_player_uid_to_id.sql
+++ database/server_player_uid_to_id.sql
@@ -13,4 +13,5 @@ ALTER FUNCTION server.player_uid_to_id(text)
   OWNER TO armalive_auto;
 GRANT EXECUTE ON FUNCTION server.player_uid_to_id(text) TO armalive_auto;
 GRANT EXECUTE ON FUNCTION server.player_uid_to_id(text) TO armalive_server;
+GRANT EXECUTE ON FUNCTION server.player_uid_to_id(text) TO armalive_reader;
 REVOKE ALL ON FUNCTION server.player_uid_to_id(text) FROM public;
