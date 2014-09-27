f82c4cab69e5220bf9e1a3e39989bf447d7148cc
MaHuJa <github@mahuja.net>
2014-09-27 04:45:55 +0200

    DB newplayer1 util fixed.

--- database/server_newplayer1.sql
+++ database/server_newplayer1.sql
@@ -1,8 +1,8 @@
-﻿-- Function: server.newplayer1(integer, text, text, numeric, text)
+﻿-- Function: server.newplayer1(integer, text, text, numeric, text[])
 
--- DROP FUNCTION server.newplayer1(integer, text, text, numeric, text);
+-- DROP FUNCTION server.newplayer1(integer, text, text, numeric, text[]);
 
-CREATE OR REPLACE FUNCTION server.newplayer1(sessionid integer, playeruid text, playerside text, jointime numeric, VARIADIC playername_p text[])
+CREATE OR REPLACE FUNCTION server.newplayer1(IN sessionid integer, IN playeruid text, IN playerside text, IN jointime numeric, VARIADIC playername_p text[])
   RETURNS void AS
 $BODY$
 DECLARE
@@ -12,19 +12,20 @@ BEGIN
 -- todo: Consider doing trigger work here
 insert into player.player(gameuid,last_name_seen) values ($2, pname);
 insert into session.sessionplayers(session, player, side, joined, playername) values 
-  ($1, server.player_uid_to_id($2), $3, server.seconds($4), pname);
+  ($1, p_id, $3, util.seconds($4), pname);
 
 -- add to playername
 update player.playername set lastseen = current_timestamp where playerid=p_id and name = pname;
 if not found then
 	insert into player.playername (playerid,name) values (p_id, pname);
 end if;
+
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE SECURITY DEFINER
   COST 100;
-ALTER FUNCTION server.newplayer1(integer, text, text, numeric, text)
-  OWNER TO armalive_auto;
-GRANT EXECUTE ON FUNCTION server.newplayer1(integer, text, text, numeric, text) TO armalive_auto;
-GRANT EXECUTE ON FUNCTION server.newplayer1(integer, text, text, numeric, text) TO armalive_server;
-REVOKE ALL ON FUNCTION server.newplayer1(integer, text, text, numeric, text) FROM public;
+ALTER FUNCTION server.newplayer1(integer, text, text, numeric, text[])
+  OWNER TO mahuja;
+GRANT EXECUTE ON FUNCTION server.newplayer1(integer, text, text, numeric, text[]) TO public;
+GRANT EXECUTE ON FUNCTION server.newplayer1(integer, text, text, numeric, text[]) TO mahuja;
+GRANT EXECUTE ON FUNCTION server.newplayer1(integer, text, text, numeric, text[]) TO armalive_auto;
