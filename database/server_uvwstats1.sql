9e21538be44fe59c345217c45e9f1a4f7dc8b215
MaHuJa <github@mahuja.net>
2014-10-20 09:06:39 +0200

    UVW stats collection enabled
    Partially addressing #26

--- database/server_uvwstats1.sql
+++ database/server_uvwstats1.sql
@@ -1,18 +1,29 @@
-﻿-- Function: server.uvwstats(integer, numeric, text, text, text, text, numeric, text[])
+﻿-- Function: server.uvwstats1(integer, numeric, text, text, text, text, numeric, text[])
 
--- DROP FUNCTION server.uvwstats(integer, numeric, text, text, text, text, numeric, text[]);
+-- DROP FUNCTION server.uvwstats1(integer, numeric, text, text, text, text, numeric, text[]);
 
-CREATE OR REPLACE FUNCTION server.uvwstats(IN sessionid integer, IN "when" numeric, IN playerid text, IN unitclass text, IN vehicleclass text, IN weaponclass text, IN weapontime numeric, VARIADIC hits text[])
+CREATE OR REPLACE FUNCTION server.uvwstats1
+(IN sessionid integer, IN "when" numeric, IN playerid text, 
+IN unitclass text, IN vehicleclass text, IN weaponclass text, 
+IN weapontime numeric, VARIADIC hits text[])
   RETURNS void AS
 $BODY$
+DECLARE
+  playerid integer = util.player_uid_to_id($3);
 BEGIN
+  EXECUTE util.checkwritable($1);
+  INSERT INTO player.uvwstats_raw (session, player, 
+    unit, vehicle, weapon, 
+    totalseconds, fired, hits)
+  VALUES ($1, playerid, 
+    $4, $5, $6, 
+    $7, $8, $9);
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE SECURITY DEFINER
   COST 100;
-ALTER FUNCTION server.uvwstats(integer, numeric, text, text, text, text, numeric, text[])
-  OWNER TO mahuja;
-GRANT EXECUTE ON FUNCTION server.uvwstats(integer, numeric, text, text, text, text, numeric, text[]) TO public;
-GRANT EXECUTE ON FUNCTION server.uvwstats(integer, numeric, text, text, text, text, numeric, text[]) TO mahuja;
-GRANT EXECUTE ON FUNCTION server.uvwstats(integer, numeric, text, text, text, text, numeric, text[]) TO armalive_auto;
-GRANT EXECUTE ON FUNCTION server.uvwstats(integer, numeric, text, text, text, text, numeric, text[]) TO armalive_server;
+ALTER FUNCTION server.uvwstats1(integer, numeric, text, text, text, text, numeric, text[])
+  OWNER TO armalive_auto;
+GRANT EXECUTE ON FUNCTION server.uvwstats1(integer, numeric, text, text, text, text, numeric, text[]) TO public;
+GRANT EXECUTE ON FUNCTION server.uvwstats1(integer, numeric, text, text, text, text, numeric, text[]) TO armalive_auto;
+GRANT EXECUTE ON FUNCTION server.uvwstats1(integer, numeric, text, text, text, text, numeric, text[]) TO armalive_server;
