66b4cc6c43f68dad19581f049285e0210bc3f2e8
MaHuJa <github@mahuja.net>
2014-10-26 17:00:11 +0100

    db uvwstats tables

---
+++ database/player_uvwstats_raw.sql
@@ -0,0 +1,25 @@
+﻿-- Table: player.uvwstats_raw
+
+-- DROP TABLE player.uvwstats_raw;
+
+CREATE TABLE player.uvwstats_raw
+(
+  session integer NOT NULL,
+  player integer NOT NULL,
+  unit text NOT NULL,
+  vehicle text NOT NULL,
+  weapon text NOT NULL,
+  totalseconds interval NOT NULL,
+  fired integer DEFAULT 0,
+  hits text[]
+)
+WITH (
+  OIDS=FALSE
+);
+ALTER TABLE player.uvwstats_raw
+  OWNER TO mahuja;
+GRANT ALL ON TABLE player.uvwstats_raw TO mahuja;
+GRANT SELECT ON TABLE player.uvwstats_raw TO armalive_reader;
+GRANT SELECT, UPDATE, INSERT ON TABLE player.uvwstats_raw TO armalive_auto;
+COMMENT ON TABLE player.uvwstats_raw
+  IS 'During the mission, the rows are all dumped in here. After the mission is over, they should be collapsed into the cooked table.';
