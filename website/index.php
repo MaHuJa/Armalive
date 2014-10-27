865ed4bc1abb2bbef61508530e743f54063d002c
MaHuJa <github@mahuja.net>
2014-10-27 18:28:40 +0100

    Index page that actually links to something

---
+++ website/index.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>Armalive - combat statistics from arma 3</title></head>
+<body>
+<p>These pages are will be "made pretty" later, for now I want things to be working.</p>
+<ul>
+<li> <a href="findplayer.php"> Find a player </a></li>
+<li> <a href="sessionlog.php"> Look up what happened in a session</a></li>
+<li> <a href="al2test.php"> Something of a "scoreboard"</a></li>
+</ul>
+</body>
+</html>
